559f0b9bccd7f989ee7e301c6e996730a1956ce1
KittyKuromi <ogawakameto@gmail.com>
2023-12-06 20:02:08 +0545

    feat: front end final code pushed

--- frontend/coin-flip/src/components/FlipOptions.tsx
+++ frontend/coin-flip/src/components/FlipOptions.tsx
@@ -1,8 +1,13 @@
 import React, { useState } from "react";
 import "./FlipOptions.css";
-import { getPublicKey, signTransaction } from "@stellar/freighter-api";
 import {
-  Keypair,
+  getPublicKey,
+  signAuthEntry,
+  signBlob,
+  signTransaction,
+} from "@stellar/freighter-api";
+
+import {
   Contract,
   Server,
   TransactionBuilder,
@@ -26,46 +31,29 @@ export const accountToScVal = (account: string) =>
   new Address(account).toScVal();
 
 export const numberToI128 = (value: number): xdr.ScVal =>
-  nativeToScVal(value, { type: "i128" });
+  nativeToScVal(value * 10 ** 7, { type: "i128" });
+
+export const StringToScVal = (value: string): xdr.ScVal =>
+  nativeToScVal(value, { type: "string" });
 
 function FlipOptions() {
   const [amount, setAmount] = useState(0);
   const [selectedOption, setSelectedOption] = useState("");
-
-  const handleOptionClicked = (option: React.SetStateAction<string>) => {
-    setSelectedOption(option);
-    console.log(option + " Clicked");
-  };
+  const [errorMessage, setErrorMessage] = useState("");
 
   async function handleTossClick() {
-    // const {
-    //   Keypair,
-    //   Contract,
-    //   Server,
-    //   TransactionBuilder,
-    //   Networks,
-    //   BASE_FEE,
-    // } = require("soroban-client");
-
-    console.log("Clicked");
     let caller = await getPublicKey();
-    console.log("value copied here");
     console.log(caller);
 
     if (selectedOption) {
-      console.log("here");
-      signTransaction;
-      
       const server = new Server("https://soroban-testnet.stellar.org", {
         allowHttp: true,
       });
       const contractAddress =
-        "CCRMHNFSSDHZHFSPZQSWWVNALILUP7R2PEWD2DL7GBVHO6VGCMFJ2FQV";
+        "CA73OANOSHMGM3TJLXW3LSRQP7NY7HIELI57XELGJSXY4OI6UYKX5ONT";
       const contract = new Contract(contractAddress);
 
-      console.log("account ok");
       const sourceAccount = await server.getAccount(caller);
-      console.log("source ok");
       let builtTransaction = new TransactionBuilder(sourceAccount, {
         fee: BASE_FEE,
         networkPassphrase: Networks.TESTNET,
@@ -73,27 +61,42 @@ function FlipOptions() {
         .addOperation(
           contract.call(
             "flip",
-            ...[accountToScVal(caller), numberToI128(amount)]
+            ...[
+              accountToScVal(caller),
+              StringToScVal(selectedOption),
+              numberToI128(amount),
+            ]
           )
         )
-        .setTimeout(30)
-        .build();
-      console.log("build ok");
+        .setTimeout(30);
 
-      let preparedTransaction = builtTransaction.toXDR();
+      let _buildTransaction = await server.prepareTransaction(
+        builtTransaction.build(),
+        Networks.TESTNET
+      );
+      let preparedTransaction = _buildTransaction.toXDR();
+
       try {
-        const signedTx = await signTransaction(preparedTransaction);
+        const signedTx = await signTransaction(preparedTransaction, {
+          networkPassphrase: Networks.TESTNET,
+          accountToSign: caller,
+        });
+        console.log(signedTx);
+
         const tx = TransactionBuilder.fromXDR(signedTx, Networks.TESTNET);
         const sendResponse = await server.sendTransaction(tx);
+        console.log(sendResponse.status);
         if (sendResponse.errorResult) {
           throw new Error("Unable to submit transaction");
         }
-      
+
         if (sendResponse.status === SendTxStatus.Pending) {
           let txResponse = await server.getTransaction(sendResponse.hash);
-      
+
           // Poll this until the status is not "NOT_FOUND"
-          while (txResponse.status === SorobanRpc.GetTransactionStatus.NOT_FOUND) {
+          while (
+            txResponse.status === SorobanRpc.GetTransactionStatus.NOT_FOUND
+          ) {
             // See if the transaction is complete
             // eslint-disable-next-line no-await-in-loop
             txResponse = await server.getTransaction(sendResponse.hash);
@@ -101,74 +104,33 @@ function FlipOptions() {
             // eslint-disable-next-line no-await-in-loop
             await new Promise((resolve) => setTimeout(resolve, 1000));
           }
-      
+
           if (txResponse.status === SorobanRpc.GetTransactionStatus.SUCCESS) {
+            console.log(txResponse);
             return txResponse.resultXdr.toXDR("base64");
           }
           // eslint-disable-next-line no-else-return
         }
         throw new Error(
-          `Unabled to submit transaction, status: ${sendResponse.status}`,
+          `Unabled to submit transaction, status: ${sendResponse.status}`
         );
-        console.log(sendResponse)
-        console.log("prapare ok");
-      } catch (error) {
-        console.log(error);
+      } catch (err) {
+        // Catch and report any errors we've thrown
+        console.log("Sending transaction failed");
+        console.log(JSON.stringify(err));
+        setErrorMessage(`Transaction failed. ${JSON.stringify(err)}`);
       }
-      // console.log("prapare ok")
-      // const signedTx = await signTransaction(preparedTransaction);
-
-      //   preparedTransaction.sign(sourceKeypair);
-      //   console.log(
-      //     `Signed prepared transaction XDR: ${preparedTransaction
-      //       .toEnvelope()
-      //       .toXDR("base64")}`
-      //   );
-      //   try {
-      //     let sendResponse = await server.sendTransaction(preparedTransaction);
-      //     console.log(`Sent transaction: ${JSON.stringify(sendResponse)}`);
-
-      //     if (sendResponse.status === "PENDING") {
-      //       let getResponse = await server.getTransaction(sendResponse.hash);
-      //       // Poll `getTransaction` until the status is not "NOT_FOUND"
-      //       while (getResponse.status === "NOT_FOUND") {
-      //         console.log("Waiting for transaction confirmation...");
-      //         // See if the transaction is complete
-      //         getResponse = await server.getTransaction(sendResponse.hash);
-      //         // Wait one second
-      //         await new Promise((resolve) => setTimeout(resolve, 1000));
-      //       }
-
-      //       console.log(
-      //         `getTransaction response: ${JSON.stringify(getResponse)}`
-      //       );
-
-      //       if (getResponse.status === "SUCCESS") {
-      //         // Make sure the transaction's resultMetaXDR is not empty
-      //         if (!getResponse.resultMetaXdr) {
-      //           throw "Empty resultMetaXDR in getTransaction response";
-      //         }
-      //         // Find the return value from the contract and return it
-      //         let transactionMeta = getResponse.resultMetaXdr;
-      //         let returnValue = transactionMeta.v3().sorobanMeta().returnValue();
-      //         console.log(`Transaction result: ${returnValue.value()}`);
-      //       } else {
-      //         throw `Transaction failed: ${getResponse.resultXdr}`;
-      //       }
-      //     } else {
-      //       throw sendResponse.errorResultXdr;
-      //     }
-      //   } catch (err) {
-      //     // Catch and report any errors we've thrown
-      //     console.log("Sending transaction failed");
-      //     console.log(JSON.stringify(err));
-      //   }
     } else {
       console.error("Please select either Heads or Tails.");
+      setErrorMessage(`Please select either Head or Tail.`);
     }
   }
 
-  const handleAmountChange = (e: { target: { value: any; }; }) => {
+  const handleOptionClicked = (option: React.SetStateAction<string>) => {
+    setSelectedOption(option);
+  };
+
+  const handleAmountChange = (e: { target: { value: any } }) => {
     const inputAmount = e.target.value;
     if (
       inputAmount === 0 ||
@@ -181,6 +143,12 @@ function FlipOptions() {
   return (
     <>
       <div className="coin-flip-container">
+        {/* Display the error message if it exists */}
+        {errorMessage && (
+          <div style={{ color: "red", margin: "20px" }}>
+            Error: {errorMessage}
+          </div>
+        )}
         <div className="input-container">
           <label>
             Enter Amount:
@@ -192,7 +160,7 @@ function FlipOptions() {
           <button
             type="button"
             className={`btn btn-primary m-4 ${
-              selectedOption === "Heads" ? "clicked" : ""
+              selectedOption === "head" ? "clicked" : ""
             }`}
             onClick={() => handleOptionClicked("head")}
           >
@@ -201,7 +169,7 @@ function FlipOptions() {
           <button
             type="button"
             className={`btn btn-primary m-4 ${
-              selectedOption === "Heads" ? "clicked" : ""
+              selectedOption === "tail" ? "clicked" : ""
             }`}
             onClick={() => handleOptionClicked("tail")}
           >
@@ -216,9 +184,9 @@ function FlipOptions() {
         <div className="toss-button-container">
           <button
             type="button"
+            disabled={!selectedOption || amount === 0}
             className="btn btn-primary m-5"
             onClick={handleTossClick}
-            disabled={selectedOption === null}
           >
             Toss
           </button>
